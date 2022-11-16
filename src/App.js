9a4c7115ae55a83c0912dbdc0306053669d2f0a8
Trepnev <trepnav21lucky@gmail.com>
2022-11-16 03:02:28 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import logo from './assets/logo.svg';
+import cart from './assets/cart.svg';
+import avatar from './assets/avatar.svg';
+import './App.scss';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <header className="header">
+                <div className="container">
+                    <div className="inner">
+                        <img src={logo} alt="logo" />
+                        <div className="info">
+                            <div className="title">React Tea</div>
+                            <div className="description">Самый лучший чай</div>
+                        </div>
+                    </div>
+                    <nav className="nav">
+                        <a href="#" className="link">
+                            <div className="cart">
+                                <img src={cart} alt="cart" />
+                                <div className="amount">
+                                    <span>1504P</span>
+                                </div>
+                            </div>
+                        </a>
+                        <div className="avatar">
+                            <a href="#" className="link">
+                                <img src={avatar} alt="avatar" />
+                            </a>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            <div className="intro">
+                <div className="container"></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
