f3004b940165412474b8aeca200a25566b80dc43
Trepnev <trepnav21lucky@gmail.com>
2022-11-16 22:33:59 +0300

    navigation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import cart from './assets/cart.svg';
 import btnPrev from './assets/btnPrev.svg';
 import btnNext from './assets/btnNext.svg';
 import avatar from './assets/avatar.svg';
+import search from './assets/search.svg';
 import './App.scss';
 
 function App() {
@@ -11,21 +12,21 @@ function App() {
         <div className="App">
             <header className="header">
                 <div className="header__container">
-                    <div className="header__inner  inner">
+                    <div className="header__inner">
                         <img src={logo} alt="logo" />
-                        <div className="inner__info">
-                            <div className="title">React Tea</div>
-                            <div className="description">Самый лучший чай</div>
+                        <div className="header__info">
+                            <div className="header__title">React Tea</div>
+                            <div className="header__description">Самый лучший чай</div>
                         </div>
                     </div>
-                    <nav className="header__nav nav">
-                        <a href="#" className="nav__cart">
+                    <nav className="header__nav">
+                        <a href="#" className="header__cart">
                             <img src={cart} alt="cart" />
                             <div className="amount">
                                 <span>1504P</span>
                             </div>
                         </a>
-                        <div className="nav__avatar">
+                        <div className="header__avatar">
                             <a href="#" className="link">
                                 <img src={avatar} alt="avatar" />
                             </a>
@@ -49,6 +50,35 @@ function App() {
                     </button>
                 </div>
             </div>
+
+            <nav className="nav">
+                <div className="nav__container">
+                    <div className="nav__bar">
+                        <a href="#" className="nav__btn  nav__btn_active">
+                            Все
+                        </a>
+                        <a href="#" className="nav__btn">
+                            Черный чай
+                        </a>
+                        <a href="#" className="nav__btn">
+                            Зеленый чай
+                        </a>
+                        <a href="#" className="nav__btn">
+                            Синий чай
+                        </a>
+                        <a href="#" className="nav__btn">
+                            Белый чай
+                        </a>
+                    </div>
+                    <div className="nav__search">
+                        <input className="nav__input" type="text" placeholder="Поиск..." />
+                        <a href="#">
+                            <img className="nav__icon" src={search} alt="search" />
+                        </a>
+                    </div>
+                </div>
+            </nav>
+
             <div className="intro">
                 <div className="intro__container"></div>
             </div>
